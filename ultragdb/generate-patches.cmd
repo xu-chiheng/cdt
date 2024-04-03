6eabc21f7bb2bacee3722e72d938e36298cd54e3
徐持恒 Xu Chiheng <chiheng.xu@gmail.com>
2024-04-03 18:25:52 +0800

    Rename generate-patches.sh.sh to generate-patches.sh.txt

--- ultragdb/generate-patches.cmd
+++ ultragdb/generate-patches.cmd
@@ -1,3 +1,3 @@
 @set PATH="%CYGWIN_DIR%\bin";%PATH%
 
-@start mintty.exe --hold=always --exec bash.exe -i generate-patches.sh.sh
+@start mintty.exe --hold=always --exec bash.exe -i generate-patches.sh.txt
